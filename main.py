ab52d185dd666eb46b158207212f70bea86b6bae
k1ut3h <hardikedu811@gmail.com>
2023-05-09 21:26:29 +0530

    optimized

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random;
+
 def quicksort(arr):
     if (len(arr)<2):
         return arr
@@ -5,7 +7,7 @@ def quicksort(arr):
         if (arr[1]<arr[0]):
             arr[0], arr[1] = arr[1], arr[0]
         return arr
-    pivot = arr[int(len(arr)/2)];
+    pivot = arr[random.randrange(0,len(arr))];
     smaller = [i for i in arr[1:] if i<=pivot]
     larger = [i for i in arr[1:] if i>pivot]
 
